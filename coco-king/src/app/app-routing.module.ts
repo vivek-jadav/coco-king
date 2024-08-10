3c9e4233be99c62a45687931ab8773e5c40548d9
vivek-jadav <vivekjadav631@gmail.com>
2024-08-10 11:44:38 +0530

    commit

--- coco-king/src/app/app-routing.module.ts
+++ coco-king/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './home/homepage/homepage.component';
+import { ProductsComponent } from './products/products.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { FoodServicesComponent } from './food-services/food-services.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'food-servies', component: FoodServicesComponent },
+  { path: 'about-us', component: AboutUsComponent },
+
+  
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- coco-king/src/app/app.module.ts
+++ coco-king/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './home/homepage/homepage.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductsComponent } from './products/products.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { FoodServicesComponent } from './food-services/food-services.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ProductsComponent,
+    RecipesComponent,
+    FoodServicesComponent,
+    AboutUsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
